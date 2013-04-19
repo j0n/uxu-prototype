5d01cc1987e2fb5b9e93cb722e1aaea9d44e0119
Jon <jon@Jons-MacBook-Pro.local>
2013-04-19 13:21:20 +0200

    adding user templates to theme

---
+++ wp-content/themes/uxu/profile-form.php
@@ -0,0 +1,74 @@
+<?php
+/*
+If you would like to edit this file, copy it to your current theme's directory and edit it there.
+Theme My Login will always look in your theme's directory first, before using this default template.
+*/
+?>
+<div class="login profile" id="theme-my-login<?php $template->the_instance(); ?>">
+	<?php $template->the_action_template_message( 'profile' ); ?>
+	<?php $template->the_errors(); ?>
+	<form id="your-profile" action="<?php $template->the_action_url( 'profile' ); ?>" method="post">
+		<?php wp_nonce_field( 'update-user_' . $current_user->ID ); ?>
+		<p>
+			<input type="hidden" name="from" value="profile" />
+			<input type="hidden" name="checkuser_id" value="<?php echo $current_user->ID; ?>" />
+		</p>
+
+		<?php if ( has_action( 'personal_options' ) ) : ?>
+
+		<h3><?php _e( 'Personal Options' ); ?></h3>
+
+		<table class="form-table">
+      <?php do_action( 'personal_options', $profileuser ); ?>
+		</table>
+
+		<?php endif; ?>
+		<?php do_action( 'profile_personal_options', $profileuser ); ?>
+		<table class="form-table">
+		<tr>
+			<th><label for="user_login"><?php _e( 'Username' ); ?></label></th>
+			<td><input type="text" name="user_login" id="user_login" value="<?php echo esc_attr( $profileuser->user_login ); ?>" disabled="disabled" class="regular-text" /> <span class="description"><?php _e( 'Your username cannot be changed.', 'theme-my-login' ); ?></span></td>
+		</tr>
+
+		<tr>
+			<th><label for="first_name"><?php _e( 'First name' ); ?></label></th>
+			<td><input type="text" name="first_name" id="first_name" value="<?php echo esc_attr( $profileuser->first_name ); ?>" class="regular-text" /></td>
+		</tr>
+
+		<tr>
+			<th><label for="last_name"><?php _e( 'Last name' ); ?></label></th>
+			<td><input type="text" name="last_name" id="last_name" value="<?php echo esc_attr( $profileuser->last_name ); ?>" class="regular-text" /></td>
+		</tr>
+
+		<tr>
+			<th><label for="nickname"><?php _e( 'Nickname' ); ?> <span class="description"><?php _e( '(required)' ); ?></span></label></th>
+			<td><input type="text" name="nickname" id="nickname" value="<?php echo esc_attr( $profileuser->nickname ); ?>" class="regular-text" /></td>
+		</tr>
+
+		</table>
+
+		<?php do_action( 'show_user_profile', $profileuser ); ?>
+		<?php
+		$show_password_fields = apply_filters( 'show_password_fields', true, $profileuser );
+		if ( $show_password_fields ) :
+		?>
+		<tr id="password">
+			<th><label for="pass1"><?php _e( 'New Password' ); ?></label></th>
+			<td><input type="password" name="pass1" id="pass1" size="16" value="" autocomplete="off" /> <span class="description"><?php _e( 'If you would like to change the password type a new one. Otherwise leave this blank.' ); ?></span><br />
+				<input type="password" name="pass2" id="pass2" size="16" value="" autocomplete="off" /> <span class="description"><?php _e( 'Type your new password again.' ); ?></span><br />
+				<div id="pass-strength-result"><?php _e( 'Strength indicator', 'theme-my-login' ); ?></div>
+				<p class="description indicator-hint"><?php _e( 'Hint: The password should be at least seven characters long. To make it stronger, use upper and lower case letters, numbers and symbols like ! " ? $ % ^ &amp; ).' ); ?></p>
+			</td>
+		</tr>
+		<?php endif; ?>
+		</table>
+
+
+		<p class="submit">
+			<input type="hidden" name="action" value="profile" />
+			<input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
+			<input type="hidden" name="user_id" id="user_id" value="<?php echo esc_attr( $current_user->ID ); ?>" />
+			<input type="submit" class="button-primary" value="<?php esc_attr_e( 'Update Profile' ); ?>" name="submit" />
+		</p>
+	</form>
+</div>
