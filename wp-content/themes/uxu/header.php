139a32d9d5cfb4afcfac9d9ce344e8598658af1c
Jon <jon@Jons-MacBook-Pro.local>
2013-04-20 19:13:44 +0200

    more responsive blogg layoout

--- wp-content/themes/uxu/header.php
+++ wp-content/themes/uxu/header.php
@@ -1,7 +1,6 @@
 <head>
   <meta charset="UTF-8">
   <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-  <meta name="viewport" content="width=device-width" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0;">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
