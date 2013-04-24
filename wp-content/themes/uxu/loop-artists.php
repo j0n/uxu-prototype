d585952e703fb6fe6a705e6b32e0075df0fee715
Jon <jon@Jons-MacBook-Pro.local>
2013-04-24 12:47:11 +0200

    adding like button to artists

--- wp-content/themes/uxu/loop-artists.php
+++ wp-content/themes/uxu/loop-artists.php
@@ -14,12 +14,19 @@
     <?php else: ?>
       </h2>
     <?php endif; ?>
+
+
     <?php if (has_post_thumbnail()) : ?>
       <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
         <?php the_post_thumbnail('large'); ?>
       </a>
     <?php endif; ?>
-    <?php the_excerpt(); ?>
+    <?php if(is_single()): ?>
+      <?php the_content(); ?>
+      <div class="fb-like" data-send="false" data-width="450" data-show-faces="false"></div>
+    <?php else: ?>
+      <?php the_excerpt(); ?>
+    <?php endif; ?>
   </article>
 <?php endwhile; ?>
 <?php endif; ?>
