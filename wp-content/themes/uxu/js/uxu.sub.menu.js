fdbe3cec25978356f94aeb04e18ad98fba725c53
Jon <jon@Jons-MacBook-Pro.local>
2013-05-07 18:03:06 +0200

    more..

--- wp-content/themes/uxu/js/uxu.sub.menu.js
+++ wp-content/themes/uxu/js/uxu.sub.menu.js
@@ -10,15 +10,17 @@
     events: function(){
       $('.menu-item').on('click', function(e) {
         e.stopPropagation();
-        $('.sub-menu').hide();
         if ($(this).find('.sub-menu').length > 0) {
           if ($(this).hasClass('current-sub-menu')) {
             $('.current-sub-menu').removeClass('current-sub-menu');
+            $('.sub-menu').hide();
           }
           else {
-            $(this).toggleClass('current-sub-menu');
+            $('.current-sub-menu').removeClass('current-sub-menu');
+            $('.sub-menu').hide();
+            $(this).addClass('current-sub-menu');
+            $(this).find('.sub-menu').show();
           }
-          $(this).find('.sub-menu').toggle();
           e.preventDefault();
         }
       });
